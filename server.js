e398f2b1fa2a4363d5b0706cb423a0d3dcac31fe
smoot <smoothtacomann@gmail.com>
2018-11-18 17:29:54 -0500

    finished server

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//refer to routes in routing folder
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
