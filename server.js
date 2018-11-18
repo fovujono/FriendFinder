d84c4901cb1b7919ab17848602258a195753f97c
smoot <smoothtacomann@gmail.com>
2018-11-18 16:50:51 -0500

    Added some html

---
+++ server.js
@@ -0,0 +1,11 @@
+//Require dependencies 
+var express = require('express');
+var path = require('path');
+
+//Setting up Express App
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
