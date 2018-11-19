47d7807746ba9d21a1736d256a0d65e838742960
smoot <smoothtacomann@gmail.com>
2018-11-18 19:58:20 -0500

    finished friends array but need to link images

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [{
+    name: "Billy May",
+    photo: "",
+    scores:[ "1","3","4","1","5","3","4","1","3","1"]
+}, {
+    name: "Lady Gaga",
+    photo:"",
+    scores: ["3","4","1","4","4","2","2","1","5","3"]
+}, {
+    name: "Shakira",
+    photo: "",
+    scores: ["5","5","5","4","1","2","2","5","5","2"]
+}, {
+    name: "Shaq",
+    photo: "",
+    scores: ["2","3","3","1","2","4","1","4","2","1"]
+},{
+    name: "Gabriel Iglesias",
+    photo: "",
+    scores:["5","2","2","3","1","3","4","4","5","2"]
+},{
+    name: "Jason Bateman",
+    photo: "",
+    scores: ["3","2","5","2","5","4","4","5","2","3"]
+} ,{
+    name: "Average Joe",
+    photo: "",
+    scores: ["1","3","4","5","3","5","1","4","5","1"],
+}]
+
+module.exports = friends;
