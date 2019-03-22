054760e216f6ea542c879c9d8544a6e727c0d046
smoot <smoothtacomann@gmail.com>
2019-03-22 16:33:12 -0400

    fixed css linking issue

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 3030;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/app/public'))
 app.use(express.json());
 
 
@@ -19,5 +20,5 @@ require("./app/routing/htmlRoutes")(app);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on: http://localhost:" + PORT);
   });
