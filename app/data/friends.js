a7d909cf9f5ff1b732bd1eab8baffbc6d0d5f54b
smoot <smoothtacomann@gmail.com>
2019-03-21 21:47:50 -0400

    changed names in data and got rid of bootstrap

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,29 @@
 var friendsArray = [{
-    "name": "Billy May",
+    "name": "Billy",
     "photo": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores":[ "1","3","4","1","5","3","4","1","3","1"]
 }, {
-    "name": "Lady Gaga",
+    "name": "Shawn White",
     "photo":"https://images.pexels.com/photos/903661/pexels-photo-903661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": ["3","4","1","4","4","2","2","1","5","3"]
 }, {
-    "name": "Shakira",
+    "name": "Shelby",
     "photo": "https://images.pexels.com/photos/206593/pexels-photo-206593.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": ["5","5","5","4","1","2","2","5","5","2"]
 }, {
-    "name": "Shaq",
+    "name": "Ralph",
     "photo": "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": ["2","3","3","1","2","4","1","4","2","1"]
 },{
-    "name": "Gabriel Iglesias",
+    "name": "Gabriel",
     "photo": "https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores":["5","2","2","3","1","3","4","4","5","2"]
 },{
-    "name": "Jason Bateman",
+    "name": "Jason",
     "photo": "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": ["3","2","5","2","5","4","4","5","2","3"]
 } ,{
-    "name": "Average Joe",
+    "name": "Joe",
     "photo": "https://images.pexels.com/photos/160914/smile-man-worker-vertical-160914.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "scores": ["1","3","4","5","3","5","1","4","5","1"],
 }];
