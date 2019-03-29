4d4085f431ebfe3f3d8ce52c7bbd75a506e4695f
smoot <smoothtacomann@gmail.com>
2019-03-29 18:41:02 -0400

    new images in data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,30 +1,30 @@
 var friendsArray = [{
     "name": "Billy",
-    "photo": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "https://damagedgoods.co.uk/wp-content/uploads/2013/05/Billyflowers.jpg",
     "scores":[ "1","3","4","1","5","3","4","1","3","1"]
 }, {
     "name": "Shawn White",
-    "photo":"https://images.pexels.com/photos/903661/pexels-photo-903661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo":"https://www.1025theriver.com/wp-content/uploads/2018/02/bigstock-Shaun-White-at-the-Guys-C-111647846.jpg",
     "scores": ["3","4","1","4","4","2","2","1","5","3"]
 }, {
     "name": "Shelby",
-    "photo": "https://images.pexels.com/photos/206593/pexels-photo-206593.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "https://prod-lippincott.imgix.net/app/uploads/2017/10/04221745/hawker_hz_2_v2.jpg?auto=false%2Ccompress&q=90&w=1040&h=740&fit=crop&crop=faces",
     "scores": ["5","5","5","4","1","2","2","5","5","2"]
 }, {
     "name": "Ralph",
-    "photo": "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "https://www.matse.psu.edu/sites/www.matse.psu.edu/files/styles/directory_photo/public/directoryphotos/ralph-colby-web.jpg?itok=JoHqFtHj",
     "scores": ["2","3","3","1","2","4","1","4","2","1"]
 },{
     "name": "Gabriel",
-    "photo": "https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "https://taubmancollege.umich.edu/sites/default/files/styles/four_column_feature/public/faculty/lg_ceuellar_web.jpg?itok=VIPaGDML",
     "scores":["5","2","2","3","1","3","4","4","5","2"]
 },{
     "name": "Jason",
-    "photo": "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "http://honolulumagazine-images.dashdigital.com/images/2014/July14/JasonM_headshot.jpg?ver=1406086171",
     "scores": ["3","2","5","2","5","4","4","5","2","3"]
 } ,{
     "name": "Joe",
-    "photo": "https://images.pexels.com/photos/160914/smile-man-worker-vertical-160914.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "photo": "https://cdn.theatlantic.com/assets/media/img/nj/2016/1/26/asian-americans-feel-held/jason-shen-headshot.jpg",
     "scores": ["1","3","4","5","3","5","1","4","5","1"],
 }];
 
